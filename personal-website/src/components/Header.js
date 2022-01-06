d0f1d7936be0c98e1408d6ba5fcccaca46e9c9ae
Fabio Schitini Alves de Oliveira <fabioschitini1@hotmail.com>
2022-01-06 16:49:11 -0300

    Update Header.js

--- personal-website/src/components/Header.js
+++ personal-website/src/components/Header.js
@@ -10,7 +10,7 @@ return(
        <Link to='/projects'> <li className='Header-list'>Projects </li> </Link> 
 
         <Link to="/contact"> <li className='Header-list'> Contact  </li></Link>    
-        <a id='CV' href='https://firebasestorage.googleapis.com/v0/b/learning-firebase-b9b2a.appspot.com/o/Web%20Developer%20Resume.pdf?alt=media&token=d1657668-075c-4b66-af78-59a06cf64814'
+        <a id='CV' href='https://www.scribd.com/document/551437771/Web-Developer-Resume'
          download='CV.pdf' target="_blank" rel="noopener noreferrer" > CV↓  <li className='Header-list'> 
 
         </li> </a>
