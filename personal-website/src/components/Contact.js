77e4875e0797d7bc4efda78189baef448229b807
Fabio Schitini Alves de Oliveira <fabioschitini1@hotmail.com>
2022-01-06 17:23:54 -0300

    Update Contact.js

--- personal-website/src/components/Contact.js
+++ personal-website/src/components/Contact.js
@@ -26,7 +26,7 @@ return(
        <p className='contact-para'>fabioschitini1@hotmail.com</p>
        <h1 className='contact-info'>Phone</h1> 
        <p className='contact-para'> +55 (75)99220-4987 </p>
-    </div>
+    </div> 
     </div>
      
 
